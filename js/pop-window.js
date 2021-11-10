c4cb4959ee3596ba6eab455e6a76f67bce1d8882
Natalia Poletaeva <91270103+NataliaPoletaeva@users.noreply.github.com>
2021-11-10 19:09:51 +0500

    Add mark-up, styling and array for pop-up project cards

---
+++ js/pop-window.js
@@ -0,0 +1,40 @@
+const arrProject = [
+    {
+        title: "Multi-Post Stories 1",
+        image: "Img Placeholder.png",
+        alternative: "Project 1",
+        description: "1. A daily selection of privately personalized reads; no accounts or sign-ups required. Lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus dolor ullam consequuntur alias, modi qui est illo eum asperiores rerum, maiores corporis aliquid ab eveniet sed, ratione at doloremque quod.",
+        technologies: ["html", "css", "ruby", "bootstrap"],
+        liveProjectBtn: "https://nataliapoletaeva.github.io/Portfolio/",
+        srcBtn: "https://github.com/NataliaPoletaeva/Portfolio"
+    },
+    {
+        title: "Multi-Post Stories 2",
+        image: "Img Placeholder.png",
+        alternative: "Project 2",
+        description: "2. A daily selection of privately personalized reads; no accounts or sign-ups required. Lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus dolor ullam consequuntur alias, modi qui est illo eum asperiores rerum, maiores corporis aliquid ab eveniet sed, ratione at doloremque quod.",
+        technologies: ["html", "css", "ruby", "bootstrap"],
+        liveProjectBtn: "https://nataliapoletaeva.github.io/Portfolio/",
+        srcBtn: "https://github.com/NataliaPoletaeva/Portfolio"
+    },
+    {
+        title: "Multi-Post Stories 3",
+        image: "Img Placeholder.png",
+        alternative: "Project 3",
+        description: "3. A daily selection of privately personalized reads; no accounts or sign-ups required. Lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus dolor ullam consequuntur alias, modi qui est illo eum asperiores rerum, maiores corporis aliquid ab eveniet sed, ratione at doloremque quod.",
+        technologies: ["html", "css", "ruby", "bootstrap"],
+        liveProjectBtn: "https://nataliapoletaeva.github.io/Portfolio/",
+        srcBtn: "https://github.com/NataliaPoletaeva/Portfolio"
+    },
+    {
+        title: "Multi-Post Stories 4",
+        image: "Img Placeholder.png",
+        alternative: "Project 4",
+        description: "4. A daily selection of privately personalized reads; no accounts or sign-ups required. Lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus dolor ullam consequuntur alias, modi qui est illo eum asperiores rerum, maiores corporis aliquid ab eveniet sed, ratione at doloremque quod.",
+        technologies: ["html", "css", "ruby", "bootstrap"],
+        liveProjectBtn: "https://nataliapoletaeva.github.io/Portfolio/",
+        srcBtn: "https://github.com/NataliaPoletaeva/Portfolio"
+    }
+]
+
+
