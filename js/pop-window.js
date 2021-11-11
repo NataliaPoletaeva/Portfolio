af28678e5905c4f10d24793f2d264c5fb73b91f2
Natalia Poletaeva <91270103+NataliaPoletaeva@users.noreply.github.com>
2021-11-11 18:30:08 +0500

    Connect the pop-up window and style it

--- js/pop-window.js
+++ js/pop-window.js
@@ -1,60 +1,69 @@
 const arrProject = [
     {
-        title: "Multi-Post Stories 1",
-        image: "Img Placeholder.png",
+        heading: "Multi-Post Stories 1",
+        image: "background/Img Placeholder.png",
         alternative: "Project 1",
-        description: "1. A daily selection of privately personalized reads; no accounts or sign-ups required. Lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus dolor ullam consequuntur alias, modi qui est illo eum asperiores rerum, maiores corporis aliquid ab eveniet sed, ratione at doloremque quod.",
+        description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
         technologies: ["html", "css", "ruby", "bootstrap"],
-        liveProjectBtn: "https://nataliapoletaeva.github.io/Portfolio/",
-        srcBtn: "https://github.com/NataliaPoletaeva/Portfolio"
     },
     {
-        title: "Multi-Post Stories 2",
-        image: "Img Placeholder.png",
+        heading: "Multi-Post Stories 2",
+        image: "background/Img Placeholder.png",
         alternative: "Project 2",
-        description: "2. A daily selection of privately personalized reads; no accounts or sign-ups required. Lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus dolor ullam consequuntur alias, modi qui est illo eum asperiores rerum, maiores corporis aliquid ab eveniet sed, ratione at doloremque quod.",
+        description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
         technologies: ["html", "css", "ruby", "bootstrap"],
-        liveProjectBtn: "https://nataliapoletaeva.github.io/Portfolio/",
-        srcBtn: "https://github.com/NataliaPoletaeva/Portfolio"
     },
     {
-        title: "Multi-Post Stories 3",
-        image: "Img Placeholder.png",
+        heading: "Multi-Post Stories 3",
+        image: "background/Img Placeholder.png",
         alternative: "Project 3",
-        description: "3. A daily selection of privately personalized reads; no accounts or sign-ups required. Lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus dolor ullam consequuntur alias, modi qui est illo eum asperiores rerum, maiores corporis aliquid ab eveniet sed, ratione at doloremque quod.",
+        description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
         technologies: ["html", "css", "ruby", "bootstrap"],
-        liveProjectBtn: "https://nataliapoletaeva.github.io/Portfolio/",
-        srcBtn: "https://github.com/NataliaPoletaeva/Portfolio"
     },
     {
-        title: "Multi-Post Stories 4",
-        image: "Img Placeholder.png",
+        heading: "Multi-Post Stories 4",
+        image: "background/Img Placeholder.png",
         alternative: "Project 4",
-        description: "4. A daily selection of privately personalized reads; no accounts or sign-ups required. Lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus dolor ullam consequuntur alias, modi qui est illo eum asperiores rerum, maiores corporis aliquid ab eveniet sed, ratione at doloremque quod.",
+        description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Lorem ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
         technologies: ["html", "css", "ruby", "bootstrap"],
-        liveProjectBtn: "https://nataliapoletaeva.github.io/Portfolio/",
-        srcBtn: "https://github.com/NataliaPoletaeva/Portfolio"
     }
 ]
 
-const techProject = document.querySelector(".pop-container");
-const popBtn = document.querySelectorAll(".see-project");
-const closeBtn = document.querySelector(".close-button");
+window.addEventListener('DOMContentLoaded', () => {
+    arrProject.forEach((project) => {
+        const div = document.createElement('div');
+        div.classList.add = 'item';
+        div.innerHTML = `<div class="item">
+                            <div class="project-card">
+                               <img src="${project.image}" alt="${project.alternative}">
+                                <div class="font-change">
+                                     <h3 class="card-head">${project.heading}</h3>
+                                     <p class="description">${project.description}</p>
+                                     <ul class="card-technologies">
+                                        ${project.technologies.map((tech) => `<li>${tech}</li>`).join('')}
+                                     </ul>
+                                    <button class="see-project" type="button">See Project</button>              
+                                </div>
+                            </div>
+                         </div>`;
+        document.querySelector('.wrapper').appendChild(div);
+    });
 
-popBtn.forEach(function (popBtn) {
-  popBtn.addEventListener("click", () => {
-    if (techProject.style.display === "none") {
-      techProject.style.display = "block";
-    } else {
-      techProject.style.display = "none";
-    }
-  });
+    const popBtn = document.querySelectorAll(".see-project");
+    const techProject = document.querySelector(".pop-container");
+
+    popBtn.forEach(function (popBtn) {
+        popBtn.addEventListener("click", () => {
+            techProject.style.display = "block";
+        });
+    });
+
+    const closeBtn = document.querySelector(".close-button");
+    closeBtn.addEventListener("click", () => {
+        techProject.style.display = "none";
+    });
 });
 
-closeBtn.addEventListener("click", () => {
-  if (techProject.style.display === "block") {
-      techProject.style.display = "none";
-  } else {
-      techProject.style.display = "block";
-  }
-});
+
+
+
