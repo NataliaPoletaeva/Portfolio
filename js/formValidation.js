d2e09dc396d4c7f94f5c34b61069732dda8d05df
Natalia Poletaeva <91270103+NataliaPoletaeva@users.noreply.github.com>
2021-11-13 14:49:11 +0500

    Fixed after linters' check

--- js/formValidation.js
+++ js/formValidation.js
@@ -2,29 +2,29 @@ const form = document.querySelector('.contact-form');
 const errorMessage = 'Email must contain only lower case characters! Try again.';
 
 function showMessage() {
-    const div = document.querySelector('.error-msg');
-    div.innerHTML = `<p>${errorMessage}</p>`;
-    div.style.display = "block"
+  const div = document.querySelector('.error-msg');
+  div.innerHTML = `<p>${errorMessage}</p>`;
+  div.style.display = 'block';
 }
 
 function validateEmail(input) {
-    const value = input.toLowerCase();
-    if (value.localeCompare(input) === 0) {
-        return true;
-    }
-    return false;
+  const value = input.toLowerCase();
+  if (value.localeCompare(input) === 0) {
+    return true;
+  }
+  return false;
 }
 
 form.addEventListener('submit', (event) => {
-    const div = document.querySelector('.error-msg');
-    event.preventDefault();
-    const email = form.elements[1];
-    const returnValue = validateEmail(email.value);
-    if (returnValue) {
-        div.remove();
-        form.submit();
-        form.reset();
-    } else {
-        showMessage();
-    }
+  const div = document.querySelector('.error-msg');
+  event.preventDefault();
+  const email = form.elements[1];
+  const returnValue = validateEmail(email.value);
+  if (returnValue) {
+    div.remove();
+    form.submit();
+    form.reset();
+  } else {
+    showMessage();
+  }
 });
